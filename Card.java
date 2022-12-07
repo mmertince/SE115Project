827e3af24d3584fb7bd6a646025479cce22fb1f6
Mustafa Mert İnce <mustafamertince@gmail.com>
2022-12-07 22:22:47 +0300

    bug fixing createDeck

--- Card.java
+++ Card.java
@@ -1,9 +1,9 @@
 public class Card{
     private char symbol;
-    private char number;
+    private String number;
     public Card(){}
     public void setSymbol(char symbol){this.symbol=symbol;}
-    public void setNumber(char number){this.number=number;}
+    public void setNumber(String number){this.number=number;}
     public char getSymbol(){return symbol;}
-    public char getNumber(){return number;}
+    public String getNumber(){return number;}
 }
--- Pisti.java
+++ Pisti.java
@@ -8,22 +8,26 @@ public class Pisti{
         Card[] deck=new Card[52];
         deck=createDeck();
         for(int i=0;i<deck.length;i++){
-            System.out.println(deck[i].getSymbol()+deck[i].getNumber());
+            System.out.println(deck[i].getSymbol()+""+deck[i].getNumber());
         }
 
 
         
     }
      public static Card[] createDeck(){
-            Card[] deck=new Card[52];;
-        char[] suits={'a','b','c','d'};
+            Card[] deck=new Card[52];
+        char[] suits={'s','c','h','d'};
         for (int i=0;i<4;i++){
             for(int j=0;j<13;j++){
                 char temp=suits[i];
                 deck[i*13+j]=new Card();
                 deck[i*13+j].setSymbol(temp);
                 int temp2=j+1;
-                deck[i*13+j].setNumber((char)temp2);
+                if (j==0)deck[i*13+j].setNumber("A");
+                else if(j==10)deck[i*13+j].setNumber("J");
+                else if(j==11)deck[i*13+j].setNumber("Q");
+                else if(j==12)deck[i*13+j].setNumber("K");
+                   else deck[i*13+j].setNumber(String.valueOf(temp2));
         }
     }
     return deck;
