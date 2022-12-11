2681572bb436d71f03e52db28a4c7135af533fc0
Mustafa Mert İnce <mustafamertince@gmail.com>
2022-12-11 22:36:33 +0300

    updating pisti.java
    i have worked on some methods which are about the flow of the game

--- Pisti.java
+++ Pisti.java
@@ -6,6 +6,10 @@ public class Pisti{
         Scanner sc=new Scanner(System.in);
         Random rd=new Random(System.currentTimeMillis());
         Card[] deck=new Card[52];
+        Card[] computerTook=new Card[52];
+        Card[] playerTook=new Card[52];
+        Card[] computerHand=new Card[4];
+        Card[] playerHand=new Card[4];
         deck=createDeck();
         for(int i=0;i<deck.length;i++){
             System.out.println(deck[i].getSymbol()+"-"+deck[i].getNumber());
@@ -33,10 +37,10 @@ public class Pisti{
                 deck[i*13+j].setSymbol(temp);
                 int temp2=j+1;
                 if (j==0)deck[i*13+j].setNumber("A");
-                else if(j==10)deck[i*13+j].setNumber("J");
-                else if(j==11)deck[i*13+j].setNumber("Q");
-                else if(j==12)deck[i*13+j].setNumber("K");
-                   else deck[i*13+j].setNumber(String.valueOf(temp2));
+                  else if(j==10)deck[i*13+j].setNumber("J");
+                   else if(j==11)deck[i*13+j].setNumber("Q");
+                    else if(j==12)deck[i*13+j].setNumber("K");
+                     else deck[i*13+j].setNumber(String.valueOf(temp2));
         }
     }
     return deck;
@@ -63,8 +67,8 @@ public class Pisti{
               else{
                 char a=deck[slctCard].getSymbol();
                 String b=deck[slctCard].getNumber();
-            shfDeck[i].setSymbol(a);
-            shfDeck[i].setNumber(b);
+                shfDeck[i].setSymbol(a);
+                shfDeck[i].setNumber(b);
         }
     }
     return shfDeck;
@@ -83,11 +87,11 @@ public class Pisti{
             cutDeck[i+26].setSymbol(a);
             cutDeck[i+26].setNumber(b);
         }
-         else{ 
-            char a=deck[i].getSymbol();
-            String b=deck[i].getNumber();
-            cutDeck[i-26].setSymbol(a);  
-            cutDeck[i-26].setNumber(b);
+           else{ 
+              char a=deck[i].getSymbol();
+              String b=deck[i].getNumber();
+              cutDeck[i-26].setSymbol(a);  
+              cutDeck[i-26].setNumber(b);
          }
     }
     return cutDeck;
@@ -115,6 +119,50 @@ public class Pisti{
         newDeck[i-4].setNumber(b);
     }
     return newDeck;
+  }
+  public static Card[] addFloorDeck(Card[] floorCards,Card thrownCard){
+    Card[] newFloorCards=new Card[floorCards.length+1];
+    char a=new char;
+    String b=new String();
+    for(int i=0;i<newFloorCards.length;i++){
+        newFloorCards[i]=new Card();
+        if(i==0){
+            a=thrownCard.getSymbol();
+            b=thrownCard.getNumber();
+            floorCards[i].setSymbol(a);
+            floorCards[i].setNumber(b);}
+          else{
+             a=floorCards[i-1].getSymbol();
+             b=floorCards[i-1].getNumber();
+             newFloorCards[i].setSymbol(a);
+             floorCards[i].setNumber(b);
+        }
+    }
+    return newFloorCards;
+
+  }
+   public static Card[] collectCars(Card[] floorCards,Card[] playerTook ){
+    Card[] newPlayerTook=new Card[playerTook.length+floorCards.length];
+    
+     for(int i=0;i<floorCards.length;i++){
+        newPlayerTook[i]=new Card();
+        char a=floorCards[i].getSymbol(0);
+        String b=floorCards[i].getNumber(null);
+        newPlayerTook[i].setSymbol(a);
+        newPlayerTook[i].setNumber(b);
+     }
+
+     for(int i=floorCards.length;i<newPlayerTook.length;i++){
+        newPlayerTook[i]=new Card();
+        char  a=playerTook[i-floorCards.length].getSymbol;
+        String b=playerTook[i-floorCards.length].getNumber;
+        newPlayerTook.setSymbol=a;
+        newPlayerTook.setNumber=b;
+
+     }
+
+       return newPlayerTook;
+
   }
 
 
